9bd53d9f055f8fae0ac070a61e0fb1996caa0729
Alex Chen <axchen7@outlook.com>
2024-01-01 14:50:52 -0800

    Fix command.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-python .\runner.py --dataset=cellphy_toy_data --cellphy_model=gt16 --n_particles=300 --num_epoch=250 --learning_rate=0.0010 --branch_prior=0.5
+python runner.py --dataset=cellphy_toy_data --cellphy_model=gt16 --n_particles=300 --num_epoch=250 --learning_rate=0.0010 --branch_prior=0.5
